47da2f50b2592b4624979158efb5e65a6a2aa994
TonyKioko <tonykioko384@gmail.com>
2018-10-17 18:30:18 +0300

    hello world test

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse, Http404,HttpResponseRedirect
+from django.shortcuts import render,redirect,get_object_or_404
 
 # Create your views here.
+# @login_required(login_url='/accounts/login/')
+def index(request):
+
+    message = "Hello World"
+
+
+    context ={"message":message}
+
+    return render(request,'index.html',context)
