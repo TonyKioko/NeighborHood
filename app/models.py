1f43d063a5929d7407c9d84cfa3af7414a1f59e8
TonyKioko <tonykioko384@gmail.com>
2018-10-18 11:57:44 +0300

    add neighborhood model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils import timezone
 # Create your models here.
+
+class neighborhood(models.Model):
+    name = models.CharField(max_length=40, null=True, blank=True)
+    location=models.CharField(max_length=100, null=True, blank=True)
+    description = HTMLField()
+    user = models.ForeignKey(User, null=True)
