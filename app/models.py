2a9ad90bb7ffb20610eeed345d5dd968bfffbbe5
TonyKioko <tonykioko384@gmail.com>
2018-10-18 12:55:26 +0300

    correct error in business model

--- app/models.py
+++ app/models.py
@@ -13,9 +13,9 @@ class Neighborhood(models.Model):
     user = models.ForeignKey(User, null=True)
 
     def save_hood(self):
-	    self.save()
+        self.save()
     def __str__(self):
-	    return self.name
+        return self.name
 class Profile(models.Model):
     bio = HTMLField(default="Noisy Neigbhors")
     photo = models.ImageField(upload_to='profpics/',default='NO IMAGE')
@@ -50,9 +50,10 @@ class Profile(models.Model):
         profile = Profile.objects.get(user = id)
         return profile
 class Business(models.Model):
-	name=models.CharField(max_length=40, null=True, blank=True)
-	user=models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
-	neighborhood=models.ForeignKey(Neighborhood, on_delete=models.CASCADE,null=True,blank=True)
-	email = models.EmailField(max_length=100, null=True, blank=True)
-
+    name=models.CharField(max_length=40, null=True, blank=True)
+    user=models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
+    neighborhood=models.ForeignKey(Neighborhood, on_delete=models.CASCADE,null=True,blank=True)
+    email = models.EmailField(max_length=100, null=True, blank=True)
     
+    def __str__(self):
+        return self.name
