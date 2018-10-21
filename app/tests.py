928b7aa98c5ea06f41003fc67ba845381210f397
TonyKioko <tonykioko384@gmail.com>
2018-10-21 14:56:21 +0300

    add nhood test

--- app/tests.py
+++ app/tests.py
@@ -5,6 +5,35 @@ import datetime as dt
 from django.utils import timezone
 
 # Create your tests here.
+class NeighborhoodTestClass(TestCase):
+    #Set up Method
+    def setUp(self):
+        '''
+        test case for profiles
+        '''
+        self.user = User(username='tony')
+        self.user.save()
+        self.hood = Neighborhood(name='Karen',location='Nairobi',description="hood of hoods",photo='photo.url',user=self.user)
+        self.hood.create_neighborhood()
+
+
+    def tearDown(self):
+        Neighborhood.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.hood,Neighborhood))
+
+    def test_save_method(self):
+        self.hood.create_neighborhood()
+        hoods = Neighborhood.objects.all()
+        self.assertTrue(len(hoods) > 0)
+
+    def test_delete_method(self):
+        self.hood.create_neighborhood()
+        self.hood.delete_neighborhood()
+        hood = Neighborhood.objects.all()
+        self.assertTrue(len(hood) == 0)
+
 class ProfileTestClass(TestCase):
     #Set up Method
     def setUp(self):
